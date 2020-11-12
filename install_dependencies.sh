b9913f16151a7d2263f450fd6fd8f0b89f6175ed
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-11-12 16:08:06 +0900

    feat: 不要なインストール処理を削除

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 docker-compose run -u $(id -u $USER):$(id -g $USER) platformio project init --ide vim --board m5stack-core-esp32
-docker-compose run -u $(id -u $USER):$(id -g $USER) platformio lib install "m5stack/M5Stack"
 
 sed -i -e 's/\/tmp\/platformio\//.\//' -e "s/\/home\/$USER\/.platformio\//.\//" .ccls
